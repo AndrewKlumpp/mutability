902e6c1299b43887c70851f229dfcc251aeabb4e
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-16 10:20:21 -0700

    finished removeEWords

--- removeEWords.js
+++ removeEWords.js
@@ -1,6 +1,14 @@
-// your code here
-
-
+function removeEWords(sentence) {
+  let newSentence = [];
+  let words = sentence.split(' ');
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    if (!word.includes("e") && !word.includes("E")) {
+      newSentence.push(word);
+    }
+  }
+  return newSentence.join(' ');
+}
 
 
 
