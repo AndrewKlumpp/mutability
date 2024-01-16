a3b1e1acbd5c5940d307e2107d1dde3605a601aa
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-16 09:37:52 -0700

    finished alternatingWords

--- alternatingWords.js
+++ alternatingWords.js
@@ -1,4 +1,13 @@
-// Your code here
+function alternatingWords(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 === 0) {
+      arr[i] = arr[i].toUpperCase();
+    } else {
+      arr[i] = arr[i].toLowerCase();
+    }
+  }
+  return arr;
+}
 
 
 
