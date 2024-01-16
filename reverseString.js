c7961094c1984ee1cbaae936a956902ee7df4a86
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-16 09:43:15 -0700

    finished reverseString

--- reverseString.js
+++ reverseString.js
@@ -1,6 +1,10 @@
 function reverseString(str) {
-
-}
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
+};
 
 
 console.log(reverseString('fish')); // 'hsif'
